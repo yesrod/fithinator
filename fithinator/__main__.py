d116ccc5a4a6208eb2f72de1204e9aabd20ff63e
Adam Dorsey <adam.dorsey@gmail.com>
2020-10-24 18:09:18 -0400

    Add arguments
    Only argument right now is config file location

--- fithinator/__main__.py
+++ fithinator/__main__.py
@@ -1,13 +1,30 @@
 from .server import Server
 from .config import Config
 
-c = Config('config.yml')
+import argparse
 
-for target in c.servers.keys():
-    server = Server(c.get_server(target))
-    info = server.get_info()
+def parse_args():
+    global parsed_args
+    p = argparse.ArgumentParser(description="FITHINATOR: Status monitor for FITH servers")
+    p.add_argument('-c', '--config',
+                        type=str,
+                        default='/boot/fithinator.yml',
+                        help='Location of the config file, default /boot/fithinator.yml'
+    )
+    parsed_args = p.parse_args()
 
-    print(info.server_name)
-    print(info.map_name)
-    print("%s/%s online" % (info.player_count, info.max_players))
-    print()
+def __main__():
+    c = Config(parsed_args.config)
+
+    for target in c.servers.keys():
+        server = Server(c.get_server(target))
+        info = server.get_info()
+
+        print(info.server_name)
+        print(info.map_name)
+        print("%s/%s online" % (info.player_count, info.max_players))
+        print()
+
+if __name__ == "__main__":
+    parse_args()
+    __main__()
