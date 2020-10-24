903246568bfe054f5f2293ac83adabc183e138ee
Adam Dorsey <adam.dorsey@gmail.com>
2020-10-24 18:14:50 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import sys
+
+if not sys.version_info[0] == 3:
+    sys.exit("ERROR: The FITHINATOR requires at least Python 3.6")
+else:
+    if sys.version_info[1] < 6:
+        sys.exit("ERROR: The FITHINATOR requires at least Python 3.6")
+
+setup(
+    name='fithinator',
+    version='0.0.1',
+    description='The FITHINATOR: A status monitor for FITH TF2 servers',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=['a2s', 'yaml'],
+    package_dir={'fithinator': 'fithinator/'}
+)
